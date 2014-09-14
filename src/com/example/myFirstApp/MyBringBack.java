1b5ca1168eeeed825ec07640b5dd31cf43eca341
Rahul Jain <rahuljain.roy@gmail.com>
2014-09-14 13:29:18 +0530

    Using the Asset Folder for Typeface-66

--- src/com/example/myFirstApp/MyBringBack.java
+++ src/com/example/myFirstApp/MyBringBack.java
@@ -6,19 +6,23 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.Align;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.view.View;
 
 public class MyBringBack extends View {
 
 	Bitmap gBall;
 	float changingY;
+	Typeface font;
 	
 	public MyBringBack(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
 		gBall = BitmapFactory.decodeResource(getResources(), R.drawable.greenball);
 		changingY = 0;
+		font = Typeface.createFromAsset(context.getAssets(), "G-Unit.ttf");
 	}
 
 	@Override
@@ -26,6 +30,15 @@ public class MyBringBack extends View {
 		// TODO Auto-generated method stub
 		super.onDraw(canvas);
 		canvas.drawColor(Color.WHITE);
+		
+		//Setting font in color
+		Paint textPaint = new Paint();
+		textPaint.setARGB(50, 254, 10, 50);
+		textPaint.setTextAlign(Align.CENTER);
+		textPaint.setTextSize(50);
+		textPaint.setTypeface(font);
+		canvas.drawText("Rahul Jain", (canvas.getWidth()/2), 200, textPaint);
+		
 		canvas.drawBitmap(gBall, (canvas.getWidth()/2), changingY, null);
 		if(changingY < canvas.getHeight()){
 			changingY+=10;

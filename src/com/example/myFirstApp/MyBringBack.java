4a6094ebdda1cd7e93829111d96207cd967ac66e
Rahul Jain <rahuljain.roy@gmail.com>
2014-09-14 13:12:35 +0530

    Animation using canvas 65

--- src/com/example/myFirstApp/MyBringBack.java
+++ src/com/example/myFirstApp/MyBringBack.java
@@ -5,17 +5,20 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 public class MyBringBack extends View {
 
 	Bitmap gBall;
+	float changingY;
 	
 	public MyBringBack(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
 		gBall = BitmapFactory.decodeResource(getResources(), R.drawable.greenball);
-		
+		changingY = 0;
 	}
 
 	@Override
@@ -23,7 +26,20 @@ public class MyBringBack extends View {
 		// TODO Auto-generated method stub
 		super.onDraw(canvas);
 		canvas.drawColor(Color.WHITE);
-		canvas.drawBitmap(gBall, (canvas.getWidth()/2), 0, null);
+		canvas.drawBitmap(gBall, (canvas.getWidth()/2), changingY, null);
+		if(changingY < canvas.getHeight()){
+			changingY+=10;
+		}
+		else{
+			changingY = 0;
+		}
+		Rect middleRect = new Rect();
+		middleRect.set(0, 400, canvas.getWidth(), 550);
+		
+		Paint ourBlue = new Paint();
+		ourBlue.setColor(Color.BLUE);
+		canvas.drawRect(middleRect, ourBlue);
+		invalidate();
 	}
 	
 	

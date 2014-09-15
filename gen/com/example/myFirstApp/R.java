fc526d8a3edc1f5d2adf8d0bec2e711bf08f2137
Rahul Jain <rahuljain.roy@gmail.com>
2014-09-15 21:07:34 +0530

    media player

--- gen/com/example/myFirstApp/R.java
+++ gen/com/example/myFirstApp/R.java
@@ -97,6 +97,7 @@ public final class R {
     }
     public static final class raw {
         public static final int radioactive=0x7f050000;
+        public static final int radioactive1=0x7f050001;
     }
     public static final class string {
         public static final int action_settings=0x7f080002;
--- src/com/example/myFirstApp/SoundStuff.java
+++ src/com/example/myFirstApp/SoundStuff.java
@@ -2,24 +2,30 @@ package com.example.myFirstApp;
 
 import android.app.Activity;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 
-public class SoundStuff extends Activity implements OnClickListener {
+public class SoundStuff extends Activity implements OnClickListener, OnLongClickListener {
 
 	SoundPool sp;
+	MediaPlayer mp;
 	int explosion=0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		View v = new View(this);
-		v.setOnClickListener((OnClickListener) this);
+		v.setOnClickListener(this);
+		v.setOnLongClickListener(this);
 		setContentView(v);
 		sp = new SoundPool(5, AudioManager.STREAM_MUSIC,0);
 		explosion = sp.load(this, R.raw.radioactive, 1);
+		
+		mp = MediaPlayer.create(this, R.raw.radioactive1);
 	}
 
 	@Override
@@ -28,6 +34,13 @@ public class SoundStuff extends Activity implements OnClickListener {
 		if(explosion !=0)
 		sp.play(explosion, 1, 1, 0, 0, 1);
 		
+	}
+
+	@Override
+	public boolean onLongClick(View v) {
+		// TODO Auto-generated method stub
+		mp.start();
+		return false;
 	}
 	
 }
